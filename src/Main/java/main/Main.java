8d183da5b63a51560185b72b9b0b47225b526414
max <pmaksi1991@gmail.com>
2022-06-10 11:10:27 +0300

    Blogengine heroku launch

--- src/Main/java/main/Main.java
+++ src/Main/java/main/Main.java
@@ -4,12 +4,11 @@ package main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SuppressWarnings("checkstyle:LeftCurly")
+//@SuppressWarnings("checkstyle:LeftCurly")
 @SpringBootApplication
 public class Main
 {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-
     }
 }
